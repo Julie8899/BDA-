37c164d07292399bcd08790d9b8c210f6dd34ab5
Julie8899 <jli10@sdsu.edu>
2021-10-16 11:25:03 -0700

    Update HW4.py
    Sorry, I accidentally create a new homework file called Homework 4 which is outside of HW4 folder. I already updated the HW4 file code inside HW4 folder which is same as Homework 4 file outside HW4 folder.

--- HW4/HW4.py
+++ HW4/HW4.py
@@ -1,42 +1,29 @@
 import math
+import sys
+from io import StringIO
 
 import numpy as np
 import pandas
 import plotly.figure_factory as ff
-
-# import pydot
+import pydot
 import statsmodels.api
-
-# from matplotlib import pyplot as plt
-# from pandas import DataFrame
+from pandas import DataFrame
 from plotly import express as px
 from plotly import graph_objects as go
-
-# import matplotlib.pyplot as plt
 from plotly.subplots import make_subplots
 from scipy import stats
-
-# from io import StringIO
-
-# from sklearn import datasets, tree
-# from sklearn.metrics import confusion_matrix
-# from sklearn.model_selection import GridSearchCV
-# from sklearn.tree import DecisionTreeClassifier, export_graphviz
-
-# import tempfile
-# from PIL import Image
-
-data_df = pandas.read_csv("suspectheartrates.csv")
+from sklearn.model_selection import GridSearchCV
+from sklearn.tree import DecisionTreeClassifier, export_graphviz
 
 
 def is_column_cat(df, column_name):
     return df[column_name].nunique() <= 2
 
 
-def plot_heatmap(df, pred_col_name, res_col_name):
-    unique_pred_values = df[pred_col_name].unique().tolist()
-    unique_res_values = df[res_col_name].unique().tolist()
-    grouped = df.groupby([pred_col_name, res_col_name])
+def plot_heatmap(df, pred_name, res_name):
+    unique_pred_values = df[pred_name].unique().tolist()
+    unique_res_values = df[res_name].unique().tolist()
+    grouped = df.groupby([pred_name, res_name])
     heatmap_z = []
     for res_value in unique_res_values:
         row = []
@@ -46,10 +33,7 @@ def plot_heatmap(df, pred_col_name, res_col_name):
     fig = ff.create_annotated_heatmap(
         heatmap_z, x=unique_pred_values, y=unique_res_values
     )
-    fig.update_layout(
-        xaxis_title=pred_col_name,
-        yaxis_title=res_col_name,
-    )
+    fig.update_layout(xaxis_title=pred_name, yaxis_title=res_name)
     fig.show()
     return
 
@@ -58,7 +42,7 @@ def plot_grouped_hist(df, cont_col_name, cat_col_name, n_bins=50):
     unique_cat_values = df[cat_col_name].unique().tolist()
     cont_value_groups = []
     calculated_bin_size = math.ceil(
-        (data_df[cont_col_name].max() - df[cont_col_name].min()) / n_bins
+        (df[cont_col_name].max() - df[cont_col_name].min()) / n_bins
     )
     print("Bin Size: " + str(calculated_bin_size))
     for cat_value in unique_cat_values:
@@ -68,17 +52,15 @@ def plot_grouped_hist(df, cont_col_name, cat_col_name, n_bins=50):
         cont_value_groups, unique_cat_values, bin_size=calculated_bin_size
     )
     fig.update_layout(
-        xaxis_title=cont_col_name,
-        legend_title=cat_col_name,
-        yaxis_title="Distribution",
+        xaxis_title=cont_col_name, legend_title=cat_col_name, yaxis_title="Distribution"
     )
     fig.show()
     return
 
 
-def plot_scatter(df, pred_col_name, res_col_name):
-    pred_values = df[pred_col_name].tolist()
-    res_values = df[res_col_name].tolist()
+def plot_scatter(df, pred_name, res_name):
+    pred_values = df[pred_name].tolist()
+    res_values = df[res_name].tolist()
 
     predictor = statsmodels.api.add_constant(pred_values)
     linear_regression_model = statsmodels.api.OLS(res_values, predictor)
@@ -89,17 +71,17 @@ def plot_scatter(df, pred_col_name, res_col_name):
 
     fig = px.scatter(x=pred_values, y=res_values, trendline="ols")
     fig.update_layout(
-        title=f"Variable: {pred_col_name}: (t-value={t_value}) (p-value={p_value})",
-        xaxis_title=pred_col_name,
-        yaxis_title=res_col_name,
+        title=f"Variable: {pred_name}: (t-value={t_value}) (p-value={p_value})",
+        xaxis_title=pred_name,
+        yaxis_title=res_name,
     )
     fig.show()
     return
 
 
-def plot_mean_sq_diff(data_df, pred_col_name, res_col_name, weighted=False, n_bins=20):
-    pred_values = data_df[pred_col_name].tolist()
-    res_values = data_df[res_col_name].tolist()
+def plot_mean_sq_diff(df, pred_name, res_name, weighted=False, n_bins=20):
+    pred_values = df[pred_name].tolist()
+    res_values = df[res_name].tolist()
 
     binned_counts, bin_edges, bin_nums = stats.binned_statistic(
         pred_values, res_values, "count", bins=n_bins
@@ -112,15 +94,15 @@ def plot_mean_sq_diff(data_df, pred_col_name, res_col_name, weighted=False, n_bi
 
     binned_mean_sq_diffs = (binned_means - res_mean) ** 2
 
-    title = f"Mean Square Diff of {res_col_name} VS Binned {pred_col_name}: "
-    secondary_y_title = f"Square Diff of {res_col_name}"
+    title = f"Mean Square Diff of {res_name} VS Binned {pred_name}: "
+    secondary_y_title = f"Square Diff of {res_name}"
     if weighted:
         weights = binned_counts / len(pred_values)
         binned_mean_sq_diffs = binned_mean_sq_diffs * weights
         title = "Weighted " + title
         secondary_y_title = "Weighted" + secondary_y_title
 
-    res_mean_sq_diff = np.nansum(binned_mean_sq_diffs) / 10
+    res_mean_sq_diff = np.nansum(binned_mean_sq_diffs) / n_bins
 
     title += str(res_mean_sq_diff)
 
@@ -132,8 +114,8 @@ def plot_mean_sq_diff(data_df, pred_col_name, res_col_name, weighted=False, n_bi
     fig.add_trace(go.Scatter(x=bin_centers, y=binned_mean_sq_diffs), secondary_y=True)
 
     fig.update_layout(title_text=title)
-    fig.update_xaxes(title_text=f"{pred_col_name}")
-    fig.update_yaxes(title_text=f"{pred_col_name} Distribution", secondary_y=False)
+    fig.update_xaxes(title_text=f"{pred_name}")
+    fig.update_yaxes(title_text=f"{pred_name} Distribution", secondary_y=False)
     fig.update_yaxes(title_text=secondary_y_title, secondary_y=True)
     fig.show()
 
@@ -145,47 +127,106 @@ def plot_mean_sq_diff(data_df, pred_col_name, res_col_name, weighted=False, n_bi
 # Input data file name: suspectheartrates.csv
 # Input comma separated predictor column names:
 # Input response column name:
+def main():
+    data_df = pandas.read_csv("suspectheartrates.csv")
+    # pred_col_names = ["heartbpm", "age", "sex"]
+    # res_col_name = "veracity"
+    pred_col_names = ["veracity", "age", "sex"]
+    res_col_name = "heartbpm"
 
-data_df = pandas.read_csv("suspectheartrates.csv")
-# pred_col_names = ["heartbpm", "age", "sex"]
-# res_col_name = "veracity"
-pred_col_names = ["veracity", "age", "sex"]
-res_col_name = "heartbpm"
+    res_is_cat = is_column_cat(data_df, res_col_name)
+    for pred_col_name in pred_col_names:
+        if res_is_cat:
+            if is_column_cat(data_df, pred_col_name):
+                # Pred is cat and Res is cat, plot heatmap
+                plot_heatmap(data_df, pred_col_name, res_col_name)
+            else:
+                # Pred is cont and Res is cat, plot grouped hist
+                # Calculate p-value and t-score, Logistic Regression
+                plot_grouped_hist(
+                    data_df,
+                    cont_col_name=pred_col_name,
+                    cat_col_name=res_col_name,
+                    n_bins=50,
+                )
 
-res_is_cat = is_column_cat(data_df, res_col_name)
-for pred_col_name in pred_col_names:
-    if res_is_cat:
-        if is_column_cat(data_df, pred_col_name):
-            # Pred is cat and Res is cat, plot heatmap
-            plot_heatmap(data_df, pred_col_name, res_col_name)
         else:
-            # Pred is cont and Res is cat, plot grouped hist
-            # Calculate p-value and t-score, Logistic Regression
-            plot_grouped_hist(
-                data_df,
-                cont_col_name=pred_col_name,
-                cat_col_name=res_col_name,
-                n_bins=50,
+            if is_column_cat(data_df, pred_col_name):
+                # Pred is cat and Res is cont, plot grouped hist
+                plot_grouped_hist(
+                    data_df,
+                    cont_col_name=res_col_name,
+                    cat_col_name=pred_col_name,
+                    n_bins=50,
+                )
+
+            else:
+                # Pred is cont and Res is cont, plot scatter with trendline
+                # Calculate p-value and t-score, Linear Regression
+                plot_scatter(data_df, pred_col_name, res_col_name)
+                plot_mean_sq_diff(
+                    data_df, pred_col_name, res_col_name, weighted=False, n_bins=15
+                )
+                plot_mean_sq_diff(
+                    data_df, pred_col_name, res_col_name, weighted=True, n_bins=15
+                )
+
+            def plot_decision_tree(decision_tree, feature_names, class_names, file_out):
+                with StringIO() as dot_data:
+                    export_graphviz(
+                        decision_tree,
+                        feature_names=feature_names,
+                        class_names=class_names,
+                        out_file=dot_data,
+                        filled=True,
+                    )
+                    graph = pydot.graph_from_dot_data(dot_data.getvalue())
+                    graph[0].write_pdf(
+                        file_out + ".pdf"
+                    )  # must access graph's first element
+                    graph[0].write_png(
+                        file_out + ".png"
+                    )  # must access graph's first element
+
+            # Continuous Features
+            continuous_features = ["heartbpm", "age"]
+            X = data_df[continuous_features].values
+
+            # Response
+            y = data_df["veracity"].values
+
+            # Decision Tree Classifier
+            max_tree_depth = 7
+            tree_random_state = 0  # Always set a seed
+            decision_tree = DecisionTreeClassifier(
+                max_depth=max_tree_depth, random_state=tree_random_state
             )
-            pass
-    else:
-        if is_column_cat(data_df, pred_col_name):
-            # Pred is cat and Res is cont, plot grouped hist
-            plot_grouped_hist(
-                data_df,
-                cont_col_name=res_col_name,
-                cat_col_name=pred_col_name,
-                n_bins=50,
+            decision_tree.fit(X, y)
+
+            plot_decision_tree(
+                decision_tree=decision_tree,
+                feature_names=continuous_features,
+                class_names=["Lie", "Truth"],
+                file_out="decisiontree",
             )
-            pass
-        else:
-            # Pred is cont and Res is cont, plot scatter with trendline
-            # Calculate p-value and t-score, Linear Regression
-            plot_scatter(data_df, pred_col_name, res_col_name)
-            plot_mean_sq_diff(
-                data_df, pred_col_name, res_col_name, weighted=False, n_bins=15
-            )
-            plot_mean_sq_diff(
-                data_df, pred_col_name, res_col_name, weighted=True, n_bins=30
-            )
-            pass
+
+    # Find an optimal tree via cross-validation
+    parameters = {
+        "max_depth": range(1, 5),
+        "criterion": ["gini", "entropy"],
+    }
+    decision_tree_grid_search = GridSearchCV(
+        DecisionTreeClassifier(random_state=tree_random_state), parameters, n_jobs=4
+    )
+    decision_tree_grid_search.fit(X=X, y=y)
+
+    cv_results = DataFrame(decision_tree_grid_search.cv_results_["params"])
+    cv_results["score"] = decision_tree_grid_search.cv_results_["mean_test_score"]
+
+    print(cv_results)
+
+    return
+
+
+if __name__ == "__main__":
+    sys.exit(main())
